896cccb5b84a114e7f414d9b1711aedc39136e39
baoquochuynh <quochuynh@lecle.co.kr>
2019-07-03 14:21:48 +0700

    add params for AppDataManager

--- mvvm-dagger2-repo-reactive/app/src/main/java/com/example/footballapp/data/AppDataManager.kt
+++ mvvm-dagger2-repo-reactive/app/src/main/java/com/example/footballapp/data/AppDataManager.kt
@@ -1,23 +1,27 @@
 package com.example.footballapp.data
 
 import android.util.Log
+import com.example.footballapp.data.db.DbHelper
+import com.example.footballapp.data.network.ApiHelper
+import com.example.footballapp.data.prefs.PrefsHelper
 import io.reactivex.Observable
 import javax.inject.Inject
 
-class AppDataManager @Inject constructor() : DataManager {
+class AppDataManager @Inject constructor(val dbHelper: DbHelper, val prefs: PrefsHelper, val apiHelper: ApiHelper) :
+    DataManager {
     override fun test() {
-        Log.e("MainActivity","AppDataManager run")
+        Log.e("MainActivity", "AppDataManager run")
     }
 
     override fun performGetUsers(): Observable<*> = null!!
 
-    override fun getCurrentUserName(): String? =""
+    override fun getCurrentUserName(): String? = ""
 
     override fun setCurrentUserName(userName: String?) {
 
     }
 
     override fun clearData() {
-
+        prefs.clearData()
     }
 }
